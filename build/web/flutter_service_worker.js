27d42e6f8800e2fd337c29b36003ff840f2d1646
youngjunFe <ab456852!!>
2025-08-28 14:42:39 +0900

    🚀 Vercel 배포용 빌드 파일 추가
    - 새로운 디자인 시스템 적용된 build/web 추가
    - Pretendard 폰트, State Layers, Elevation 시스템
    - vercel.json 설정 업데이트

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "edfecc3c7310c5945d07456b8493c8c5",
+const RESOURCES = {"flutter_bootstrap.js": "c14a6584d34b5dd3358309f6af814b6d",
 "version.json": "75ad752899be5646e832843c0e4bef46",
-"index.html": "2d93ecac3ad6e3f940e13646c8589d50",
-"/": "2d93ecac3ad6e3f940e13646c8589d50",
-"main.dart.js": "71a1134eb627f3db6f2b2b8a624bb4bf",
+"index.html": "1a218ad60df8c9374f7ddcb2a354eb9a",
+"/": "1a218ad60df8c9374f7ddcb2a354eb9a",
+"main.dart.js": "7fa9b65e9f8fc0f9b15d9018b1fd246a",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
