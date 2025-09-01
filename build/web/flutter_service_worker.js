400c570a44b253090bb3aaff28b462e29b556285
youngjunFe <ab456852!!>
2025-09-01 14:16:23 +0900

    웹 빌드 업데이트

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c337538d71c323fceb4f76bef75a2eaf",
+const RESOURCES = {"flutter_bootstrap.js": "277b3f3433c70440c5ee0a8ad10e1800",
 "version.json": "75ad752899be5646e832843c0e4bef46",
 "index.html": "1a218ad60df8c9374f7ddcb2a354eb9a",
 "/": "1a218ad60df8c9374f7ddcb2a354eb9a",
-"main.dart.js": "f790f14bead1bef429f3c0b555c98cc9",
+"main.dart.js": "5fd40229a8c49a8979134ad41f41c0d8",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
