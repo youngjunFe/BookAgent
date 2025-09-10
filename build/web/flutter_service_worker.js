aaa127a9575d532f2198b84d4fd1fab0fb93e4c2
youngjunFe <ab456852!!>
2025-09-10 15:03:39 +0900

    ACTUALLY fix slide images - replace random URLs with real assets
    - Finally replace https://picsum.photos random URLs with actual asset paths
    - Change Image.network to Image.asset for proper local asset loading
    - Fix variable references from tempImages to slideImages
    - This will actually show the uploaded slide images instead of random photos

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1971071e125521b263a3bd270c024bac",
+const RESOURCES = {"flutter_bootstrap.js": "4ae98abb2f74b2cb431dd4263f9c58ea",
 "version.json": "75ad752899be5646e832843c0e4bef46",
 "index.html": "1a218ad60df8c9374f7ddcb2a354eb9a",
 "/": "1a218ad60df8c9374f7ddcb2a354eb9a",
-"main.dart.js": "5cc9a050a9164de87017c893fbe301c7",
+"main.dart.js": "46c7921d68664697d90605cdf6d16ed3",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -131,10 +131,10 @@ class _HomeViewState extends State<HomeView> {
 
   // 전체 화면을 차지하는 캐러셀 위젯
   Widget _buildFullScreenCarousel() {
-    final List<String> tempImages = [
-      'https://picsum.photos/400/600?random=1',
-      'https://picsum.photos/400/600?random=2', 
-      'https://picsum.photos/400/600?random=3',
+    final List<String> slideImages = [
+      'assets/images/slides/Group 26086144.png',
+      'assets/images/slides/Group 26086146.png',
+      'assets/images/slides/onboarding_01.png',
     ];
 
     return Container(
@@ -153,7 +153,7 @@ class _HomeViewState extends State<HomeView> {
                 _currentPage = index;
               });
             },
-            itemCount: tempImages.length,
+            itemCount: slideImages.length,
             itemBuilder: (context, index) {
               return Container(
                 decoration: BoxDecoration(
@@ -162,8 +162,8 @@ class _HomeViewState extends State<HomeView> {
                 ),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(16),
-                  child: Image.network(
-                    tempImages[index],
+                  child: Image.asset(
+                    slideImages[index],
                     fit: BoxFit.cover,
                     width: double.infinity,
                     height: double.infinity,
@@ -239,7 +239,7 @@ class _HomeViewState extends State<HomeView> {
             child: Center(
               child: GestureDetector(
                 onTap: () {
-                  if (_currentPage < tempImages.length - 1) {
+                  if (_currentPage < slideImages.length - 1) {
                     _pageController.nextPage(
                       duration: const Duration(milliseconds: 300),
                       curve: Curves.easeInOut,
