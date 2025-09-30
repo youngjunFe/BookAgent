a5746ba407e9cca05a528ebc73db5799fc30718d
youngjunFe <ab456852!!>
2025-09-30 14:48:09 +0900

    Replace terms/privacy dialogs with Notion page links - cleaner UX

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "71e062abebd6f59592ee9a8944a7d531",
+const RESOURCES = {"flutter_bootstrap.js": "c1ecff2e4e3b9abe7dbf29e9480d9137",
 "version.json": "75ad752899be5646e832843c0e4bef46",
 "index.html": "1dc908874b4643deb7165ddc3262f83d",
 "/": "1dc908874b4643deb7165ddc3262f83d",
-"main.dart.js": "8cc566df746e4c6ca47ac2900dfd5613",
+"main.dart.js": "cc61fe6875e0e7f3118e96f4f2ed810e",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 ".netlify/state.json": "99ba3e3db32fc6790c8c0f423f1b8926",
