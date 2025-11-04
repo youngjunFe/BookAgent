df9fc4ddcf4ae9406493639b5d0f110f0d55c8b9
youngjunFe <ab456852!!>
2025-11-04 15:19:49 +0900

    Update web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "487e56148b149ab5d3b46ce80cde0903",
+const RESOURCES = {"flutter_bootstrap.js": "ba3e08ca44c3822ad268061d2132e78b",
 "version.json": "2c5d98e6757bee452eddf0f2aa33ee89",
 "index.html": "23cc3a7d839248bd77e957b37d6600a3",
 "/": "23cc3a7d839248bd77e957b37d6600a3",
-"main.dart.js": "46f8203d4a51cc7707320be16a91b60e",
+"main.dart.js": "7abb425384ab471d3de55ae9bfe19c1e",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
